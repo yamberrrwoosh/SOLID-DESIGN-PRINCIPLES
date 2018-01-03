02e30becb2d9af8113e13f7bfbb06781219f4a30
jmgalicia <jmgalicia14@gmail.com>
2018-01-03 00:06:31 -0500

    initial

---
+++ SOLID-Scala/src/main/test/singleresponsibility/before/FileReaderWriterIfElseTest.scala
@@ -0,0 +1,10 @@
+package singleresponsibility.before
+
+import org.scalatest.FunSuite
+import org.scalatest.BeforeAndAfter
+import org.scalatest.mock.MockitoSugar
+import org.mockito.Mockito._
+
+class FileReaderWriterIfElseTest extends FunSuite with BeforeAndAfter with MockitoSugar {
+  
+}
